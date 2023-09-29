421c81b9136751f9ebe284fa80b5aee07ee747bd
Kanak Das <kdas006@ucr.edu>
2023-09-29 15:46:16 -0700

    sec1

---
+++ sec01/sec01-errors.ml
@@ -0,0 +1,22 @@
+(* Section 1: Some Errors *)
+
+(* This program has several errors in it so we can try to debug them. *)
+
+let x = 34
+
+y = x + 1
+
+let z = if y then 34 else x < 4
+
+let q = if y > 0 then 0
+
+let w = 0
+
+let let = 34
+
+let v = 4.0 / 2.0
+
+let u = x / w
+
+let 0cse341 = true
+
